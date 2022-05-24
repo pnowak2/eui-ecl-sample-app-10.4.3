b70ae59690189182f05cc4d2a1e91b74d5d2a2c0
Piotr Nowak <p.nowak2@gmail.com>
2022-05-24 11:03:42 +0200

    example

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { EclDatePickerDatePickedEvent } from '@eui/ecl-core';
 
 @Component({
     templateUrl: './home.component.html',
 })
 export class HomeComponent {
+    PUBLICATION_DATE_TO = 'some fake date constant..';
+
+    onDatePicked(evt: EclDatePickerDatePickedEvent, pubDate: string, someBoolean: boolean) {
+        console.log(evt, pubDate, someBoolean);
+    }
 
 }
